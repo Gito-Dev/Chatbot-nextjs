e922a992ca7c9742069312e570cde89b9f9d985b
Gito Dev <gitodevelopment@gmail.com>
2025-02-04 21:46:03 +0100

    fix

--- src/app/components/Chat/ChatButton.jsx
+++ src/app/components/Chat/ChatButton.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.png";
 export const ChatButton = ({ onClick }) => {
   return (
     <button
-      className="w-[55px] h-[55px] bg-blue-500 text-white rounded-full shadow-lg relative overflow-hidden hover:scale-110 transition-transform duration-200 border-2 border-blue-600"
+      className="w-[60px] h-[60px] bg-blue-500 text-white rounded-full shadow-lg relative overflow-hidden hover:scale-110 transition-transform duration-200 border-2 border-blue-600"
       onClick={onClick}
     >
       <Image 
